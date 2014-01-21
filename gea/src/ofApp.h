281b263a422643e9b1ddb75d0c6bf98241913559
Aris Bezas <igoumeninja@gmail.com>
2014-01-21 17:51:21 +0200

    Add gea with OSC

---
+++ gea/src/ofApp.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxLua.h"
+
+//******************//
+//****** GEA *******//
+#include "ofxOsc.h"
+#define HOST "localhost"
+#define LuaReceiverPort 46100
+#define NUM_MSG_STRINGS 20
+//******************//
+
+#include "ofxLuaBindings.h"	// the OF api -> lua binding
+
+class ofApp : public ofBaseApp, ofxLuaListener {
+    
+public:
+    
+    // main
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    // input
+    void keyPressed(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    
+    // ofxLua error callback
+    void errorReceived(string& msg);
+    
+    // a bunch of api tests
+    void runTests();
+    
+    // script control
+    void reloadScript();
+    void nextScript();
+    void prevScript();
+    
+    ofxLua lua;
+    vector<string> scripts;
+    int currentScript;
+    
+    
+    
+    //******************//
+    //****** GEA *******//
+
+    void runScript(int scriptID);
+
+    ofxOscReceiver receiver;
+    ofxOscSender	luaSender;
+    
+    int current_msg_string;
+    string msg_strings[NUM_MSG_STRINGS];
+    float timers[NUM_MSG_STRINGS];
+    string scriptDirectory;
+    int scriptID;
+    //******************//
+    
+    
+};
